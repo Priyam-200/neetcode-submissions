41d30a9d68a1231f34327d8913af37083ed3cf0f
Priyam-200 <priyamr819@gmail.com>
2026-07-22 14:55:18 +0530

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.cpp
@@ -0,0 +1,87 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// class Solution {
+// public:
+//     void insertAtHead(ListNode* &head,ListNode* &tail,ListNode* &curr){
+//         if(head==NULL && tail==NULL){
+//             head=curr;
+//             tail=curr;
+//         }
+//         else{
+//             tail->next=curr;
+//             tail=curr;
+//         }
+//     }
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         ListNode* head=NULL;
+//         ListNode* tail=NULL;
+//         while(list1!=NULL || list2!=NULL){
+//             bool ans1=(list1==NULL?false:true);
+//             bool ans2=(list2==NULL?false:true);
+//             if(ans1 && ans2){
+//                 if(list1->val<list2->val){
+//                     ListNode* curr=list1;
+//                     list1=list1->next;
+//                     curr->next=NULL;
+//                     insertAtHead(head,tail,curr);
+//                 }
+//                 else{
+//                     ListNode* curr=list2;
+//                     list2=list2->next;
+//                     curr->next=NULL;
+//                     insertAtHead(head,tail,curr);
+//                 }
+//             }
+//             else if(ans1){
+//                 ListNode* curr=list1;
+//                 list1=list1->next;
+//                 curr->next=NULL;
+//                 insertAtHead(head,tail,curr);
+//             }
+//             else if(ans2){
+//                 ListNode* curr=list2;
+//                 list2=list2->next;
+//                 curr->next=NULL;
+//                 insertAtHead(head,tail,curr);
+//             }
+//         }
+//         return head;
+//     }
+// };
+
+
+
+
+
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL && list2==NULL){
+            return NULL;
+        }
+        if(list1==NULL){
+            return list2;
+        }
+        if(list2==NULL){
+            return list1;
+        }
+        if(list1->val<=list2->val){
+            list1->next=mergeTwoLists(list1->next,list2);
+            return list1;
+        }
+        else{
+            list2->next=mergeTwoLists(list1,list2->next);
+            return list2;
+        }
+    }
+};
